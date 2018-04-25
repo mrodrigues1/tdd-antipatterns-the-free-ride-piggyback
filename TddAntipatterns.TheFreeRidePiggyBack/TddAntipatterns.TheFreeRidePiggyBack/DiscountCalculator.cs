252bb0aff765fc27f9a466bd5e070105a6968efc
Matheus Rodrigues <matheus.moreira.rodrigues01@gmail.com>
2018-04-25 00:00:52 -0300

    Refactors

--- TddAntipatterns.TheFreeRidePiggyBack/TddAntipatterns.TheFreeRidePiggyBack/DiscountCalculator.cs
+++ TddAntipatterns.TheFreeRidePiggyBack/TddAntipatterns.TheFreeRidePiggyBack/DiscountCalculator.cs
@@ -2,17 +2,9 @@
 {
     public class DiscountCalculator
     {
-        public decimal CalculateDiscount1(int age)
+        public decimal CalculateDiscount(bool firstPurchase, int age)
         {
-            if (age > 60)
-                return 0.2M;
-
-            return 0;
-        }
-
-        public decimal CalculateDiscount(int age, bool firtPurchase)
-        {
-            if (firtPurchase)
+            if (firstPurchase)
                 return 0.1M;
             else if (age > 60)
                 return 0.2M;
--- TddAntipatterns.TheFreeRidePiggyBack/TddAntipatterns.TheFreeRidePiggyBack/DiscountCalculatorTests.cs
+++ TddAntipatterns.TheFreeRidePiggyBack/TddAntipatterns.TheFreeRidePiggyBack/DiscountCalculatorTests.cs
@@ -1,61 +1,59 @@
-using System;
 using Xunit;
 
 namespace TddAntipatterns.TheFreeRidePiggyBack
 {
     public class DiscountCalculatorTests
     {
-        [Fact]
-        public void Test1()
-        {
-            //Arrange
-            int age = 65;
-            decimal expectedResult = 0.2M;
+[Fact]
+public void CalculateDiscount_ExpectedDiscountForFirstTimePurchase()
+{
+    //Arrange            
+    decimal expected = 0.1M;
+    decimal expectedResultAge = 0.2M;
 
-            var sut = new DiscountCalculator();
+    var sut = new DiscountCalculator();
 
-            //Act
-            var result = sut.CalculateDiscount1(age);
+    //Act
+    var result = sut.CalculateDiscount(true, 30);
+    var resultAge = sut.CalculateDiscount(false, 65);
 
-            //Assert
-            Assert.Equal(expectedResult, result);
-        }
+    //Assert            
+    Assert.Equal(expected, result);
+    Assert.Equal(expectedResultAge, resultAge);
+}
 
-        [Fact]
-        public void Test2()
-        {
-            //Arrange
-            int age = 65;
-            bool firstPurchase = true;
-            decimal expected = 0.2M;
-            decimal expectedResultFirstPurchase = 0.1M;
+[Fact]
+public void CalculateDiscount_FirstPurchaseTrueAsParameter_Expected10PercentDiscount()
+{
+    //Arrange
+    int age = 30;
+    bool firstPurchase = true;
+    decimal expectedResult = 0.1M;
 
-            var sut = new DiscountCalculator();
+    var sut = new DiscountCalculator();
 
-            //Act
-            var result = sut.CalculateDiscount(age, false);
-            var resultFirstPurchase = sut.CalculateDiscount(30, firstPurchase);
+    //Act
+    var result = sut.CalculateDiscount(firstPurchase, age);
 
-            //Assert
-            Assert.Equal(expected, result);
-            Assert.Equal(expectedResultFirstPurchase, resultFirstPurchase);
-        }
+    //Assert
+    Assert.Equal(expectedResult, result);
+}
 
-        [Fact]
-        public void Test3()
-        {
-            //Arrange
-            int age = 30;
-            bool firstPurchase = true;
-            decimal expectedResult = 0.1M;
+[Fact]
+public void CalculateDiscount_Age65AsParameter_Expected20PercentDiscount()
+{
+    //Arrange
+    int age = 64;
+    bool firstPurchase = false;
+    decimal expectedResult = 0.2M;
 
-            var sut = new DiscountCalculator();
+    var sut = new DiscountCalculator();
 
-            //Act
-            var result = sut.CalculateDiscount(age, firstPurchase);
+    //Act
+    var result = sut.CalculateDiscount(firstPurchase, age);
 
-            //Assert
-            Assert.Equal(expectedResult, result);
-        }
+    //Assert
+    Assert.Equal(expectedResult, result);
+}
     }
 }
